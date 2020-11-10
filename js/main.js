530ccca2bc7c55a610098cce5defb21902d6340e
Davide Benazzi <benazzi.davide@gmail.com>
2020-11-10 15:49:18 +0100

    V-if , ma non funziona

--- js/main.js
+++ js/main.js
@@ -8,6 +8,10 @@ const app = new Vue ({
       'Fare il bucato',
     ],
     message: '',
+    endList: true,
+  },
+  created() {
+    setInterval( this.controlList() , 1000 );
   },
   methods: {
     submit() {
@@ -16,6 +20,13 @@ const app = new Vue ({
     },
     remove(index) {
       this.todoList.splice(index , 1);
+      // this.controlList();
+    },
+    controlList() {
+      if (this.todoList.length === 0) {
+        this.endList = false;
+      }
+      this.endList = true;
     },
   },
 });
